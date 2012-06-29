62d3623762c3a53ca59907dd15c87cfc6acb4501
Georg Ringer <mail@ringerge.org>
2012-06-29 13:10:24 +0200

    changed email address

--- Classes/Service/DatabaseApiService.php
+++ Classes/Service/DatabaseApiService.php
@@ -2,7 +2,7 @@
 /***************************************************************
  *  Copyright notice
  *
- *  (c) 2012 Georg Ringer <typo3@ringerge.org>
+ *  (c) 2012 Georg Ringer <georg.ringer@cyberhouse.at>
  *  All rights reserved
  *
  *  This script is part of the TYPO3 project. The TYPO3 project is
