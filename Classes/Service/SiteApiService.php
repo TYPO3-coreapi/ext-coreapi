6ef4612f114d9b5c7f5b0a745754ddac54ba4d99
Georg Ringer <mail@ringerge.org>
2012-07-03 06:06:26 +0200

    Start SiteApiService

---
+++ Classes/Service/SiteApiService.php
@@ -0,0 +1,40 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Georg Ringer <georg.ringer@cyberhouse.at>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Site API service
+ *
+ * @package TYPO3
+ * @subpackage tx_coreapi
+ */
+class Tx_Coreapi_Service_SiteApiService {
+
+	/*
+	 * site:info					Provides some basic site status (Version, file size ...)
+	 * site:sysNews					Create a sys news, e.g. for new deployment
+	 */
+
+}
+
+?>
