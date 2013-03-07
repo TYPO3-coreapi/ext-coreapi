5c96b4d1c6de13da176bce5f4292184ab9d3de13
Helmut Hummel <h.hummel@bitmotion.de>
2013-03-07 17:13:35 +0100

    [TASK] Include cache and category tables if available
    In newer TYPO3 versions the caching framework
    generates the table definitions dynamically.
    
    Since 6.0 the category registry also dynamically
    adds fields.
    
    Add both to the table definitions if the TYPO3 API
    calls are available.

--- Classes/Service/DatabaseApiService.php
+++ Classes/Service/DatabaseApiService.php
@@ -86,6 +86,14 @@ class Tx_Coreapi_Service_DatabaseApiService {
 			}
 		}
 
+		if (is_callable('t3lib_cache::getDatabaseTableDefinitions')) {
+			$tblFileContent .= t3lib_cache::getDatabaseTableDefinitions();
+		}
+
+		if (class_exists('TYPO3\\CMS\\Core\\Category\\CategoryRegistry')) {
+			$tblFileContent .= \TYPO3\CMS\Core\Category\CategoryRegistry::getInstance()->getDatabaseTableDefinitions();
+		}
+
 		if ($tblFileContent) {
 			$fileContent = implode(LF, $this->sqlHandler->getStatementArray($tblFileContent, 1, '^CREATE TABLE '));
 			$FDfile = $this->sqlHandler->getFieldDefinitions_fileContent($fileContent);
