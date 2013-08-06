25794cdc9a57328d29fd275b86e09d37513b813c
Tobias Liebig <work@etobi.de>
2013-08-06 11:54:06 +0200

    [!!!][TASK] enforce usage of CommandController, if available
    * do not provide the "old style" "coreapi" commands in > 4.6

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,17 +1,19 @@
 <?php
 if (!defined ('TYPO3_MODE')) die ('Access denied.');
 
-if (TYPO3_MODE === 'BE') {
-		// Register commands
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_Coreapi_Command_DatabaseApiCommandController';
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_Coreapi_Command_CacheApiCommandController';
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_Coreapi_Command_SiteApiCommandController';
-	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_Coreapi_Command_ExtensionApiCommandController';
+if (class_exists('Tx_Extbase_MVC_Controller_CommandController')) {
+	if (TYPO3_MODE === 'BE') {
+			// Register commands (available since 4.6)
+		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_Coreapi_Command_DatabaseApiCommandController';
+		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_Coreapi_Command_CacheApiCommandController';
+		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_Coreapi_Command_SiteApiCommandController';
+		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'Tx_Coreapi_Command_ExtensionApiCommandController';
+	}
+} else {
+		// Register the CLI dispatcher
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['cliKeys'][$_EXTKEY] = array(
+		'EXT:' . $_EXTKEY . '/Scripts/Cli.php', '_CLI_lowlevel'
+	);
 }
-
-	// Register the CLI dispatcher
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['cliKeys'][$_EXTKEY] = array(
-	'EXT:' . $_EXTKEY . '/Scripts/Cli.php', '_CLI_lowlevel'
-);
 
 ?>
